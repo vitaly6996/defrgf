d0c8cd50bc9257d21e7e07629232dada596ad931
Шалтыков Виталий Александрович <316221@college.local>
2019-04-15 09:55:34 +0300

    1 save

---
+++ ConsoleApplication1/Footwear.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Footwear
+    {
+        string prod;
+        float size;
+        double price;
+    }
+}
